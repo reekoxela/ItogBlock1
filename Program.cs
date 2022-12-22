95c83f16fc8778436ae7b37274759e67f3f45df0
Alex Ivanov <reekoxela@yandex.ru>
2022-12-22 09:55:34 +0300

    Added array input/output

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write($"Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+string[] mass = new string [size];
+
+InitArray(mass);
+
+Console.WriteLine($"Массив: [{String.Join(",",mass)}]");
+
+//******************************************************МЕТОДЫ****************************************************
+//Ввод массива с клавиатуры
+void InitArray(string[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine()!;
+    }
+    Console.WriteLine();
+}
